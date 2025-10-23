0044af117cf07d8e89fbce171b18ac2da77af110
Chris Owen <c.ornine@gmail.com>
2025-10-23 15:33:41 +0700

    feat: initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { fromLonLat } from 'ol/proj';
+import { ImageTile } from 'ol/source';
+
+function imageTileFunction(z, x, y) {
+  return `./tiles/${z}_${x}_${y}.jpg`
+}
+
+const baseTileLayerImage = new TileLayer({
+  source: new ImageTile({
+    url: imageTileFunction,
+  })
+})
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    baseTileLayerImage
+  ],
+  view: new View({
+    // center: [0, 0],
+    center: fromLonLat([-100, 60]),
+    zoom: 2,
+    minZoom: 3,
+    maxZoom: 5,
+  })
+});
